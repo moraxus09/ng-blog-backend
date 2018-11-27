55695bb75566627687df28d28862e0d1f95331a9
Ivan Makhlynets <ivan.makhlynets@boost.solutions>
2018-11-27 15:19:31 +0200

    changed multiparty to multer

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,53 +1,40 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const multiparty = require('multiparty');
+const multer = require('multer');
 const path = require('path');
-const fs = require('fs-extra');
 const _ = require('lodash');
 const User = require('../models/user');
 const config = require('../app.config');
 
-
-router.post('/register', (req, res) => {
-    
-    const form = new multiparty.Form();
-
-    new Promise((resolve, reject) => {
-        form.parse(req, (err, fields, files) => {
-            if (!err) {
-                const user = {};
-                for (const key in fields) {
-                    user[key] = fields[key][0];
-                }
-                user.password = bcrypt.hashSync(user.password, 8);
-
-                if (files['avatar']) {
-                    const avatar = files['avatar'][0];
-                    const fileName = Date.now() + path.extname(avatar.originalFilename);
-                    fs.moveSync(avatar.path, 'public/profile-avatars/' + fileName);
-                    user.avatarSrc = '/profile-avatars/' + fileName;
-                };
-
-                resolve(user);
-            } else {
-                reject(err);
-            }
-        })
-    })
-    .then(user => {
-        return User.create(user, (err, user) => {
-            if (err) {
-                return res.sendStatus(500);
-            }
-    
-            const token = jwt.sign({id: user._id}, config.secret, {expiresIn: 86400});
-    
-            res.send({token, user: _.omit(user.toObject(), 'password')});
-        });
-    })
-    .catch(() => res.sendStatus(500));
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/profile-avatars/');
+    },
+    filename: (req, file, cb) => {
+        const fileName = file.fieldname + Date.now() + path.extname(file.originalname);
+        cb(null, fileName);
+    }
 });
+
+router.post('/register', multer({storage}).single('avatar'), (req, res) => {
+    const user = {
+        ...req.body,
+        password: bcrypt.hashSync(req.body.password, 8)
+    };
+
+    if (req.file) {
+        user.avatarSrc = '/profile-avatars/' + req.file.filename;
+    }
+
+    User.create(user, (err, user) => {
+        if (err) {
+            return res.sendStatus(500);
+        }
+        const token = jwt.sign({id: user._id}, config.secret, {expiresIn: 86400});
+        res.send({token, user: _.omit(user.toObject(), 'password')});
+    });
+})
 
 router.post('/login', (req, res) => {
 
